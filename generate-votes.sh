1124236a2ca5bb80210599e6ccc3a75c21c0283e
Azii1 <166354827+Azii1@users.noreply.github.com>
2025-01-09 12:04:09 +0100

    Update generate-votes.sh

--- generate-votes.sh
+++ generate-votes.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+echo "starting ApacheBench to generate votes..."
 
-# create 3000 votes (2000 for option a, 1000 for option b)
+# create 2000 votes (2000 for option a, 1000 for option b)
 ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" http://vote/
 ab -n 1000 -c 50 -p postb -T "application/x-www-form-urlencoded" http://vote/
-ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" http://vote/
+
+# create 1000 votes for option B
+ab-n 1000 -c 50 -p postb -T "application/x-www-form-urlencoded" http://vote/
+
+echo "vote generation completed."
